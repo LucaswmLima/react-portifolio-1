2824b4791544129a79bed1cc080e1f57d52d9644
Lucas William <lucaswilliamml@gmail.com>
2023-07-22 03:38:47 -0300

    style and skill changes

--- src/components/Skills/SkillsList.jsx
+++ src/components/Skills/SkillsList.jsx
@@ -2,12 +2,12 @@
 export const SkillsList = [
     { title: 'HTML', percentage: 100 },
     { title: 'CSS', percentage: 90 },
-    { title: 'Javascript', percentage: 75 },
-    { title: 'React', percentage: 75 },
-    { title: 'Python', percentage: 75 },
-    { title: 'C/C++', percentage: 75 },    
-    { title: 'Flutter', percentage: 75 },
-    { title: 'Node.js', percentage: 50 },
+    { title: 'Javascript', percentage: 70 },
+    { title: 'React', percentage: 70 },
+    { title: 'Python', percentage: 70 },
+    { title: 'C/C++', percentage: 70 },    
+    { title: 'Flutter', percentage: 70 },
+    { title: 'Node.js', percentage: 70 },
     { title: 'Java', percentage:30 },
 
   ];
