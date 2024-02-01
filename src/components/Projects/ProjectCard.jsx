3c4c5388cb222203c0083b581975cefbda7913cd
Lucas William <lucaswilliamml@gmail.com>
2024-01-31 21:14:11 -0300

    project changes

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -8,46 +8,58 @@ export const ProjectCard = ({ title, description, technologies, imgUrl, linkOnli
 
   return (
     <Container className="project-card-container">
-    <Row  className=" project-card justify-content-center">
-        <div className="project-card-img">
-          <img src={imgUrl} />
-        </div>
-      <Col className="project-info justify-content-center" md={6} sm={12}>
-        <Row className="project-title"> <h4>{title}</h4></Row>
-        <Row className="project-description"><p>{description}</p></Row>
-        <Row md="auto" sm="auto" className="project-techs justify-content-center flex-nowrap">
-  {technologies && technologies.length > 0 ? (
-    technologies.map((item, index) => (
-      <Col key={index} className="text-center">
-        <p>{item}</p>
-      </Col>
-    ))
-  ) : (
-    <p></p>
-  )}
-</Row>
-       
-        <Row
-          className={` align-items-end projects-buttons ${showOnlyOneButton ? "justify-content-center" : ""
+      <Row className=" project-card justify-content-center">
+        <Col className="project-info justify-content-center" md={6} sm={12}>
+          <Row>
+            <div className="project-card-img">
+              <img src={imgUrl} />
+            </div>
+          </Row>
+          <Row className="project-title">
+            {" "}
+            <h4>{title}</h4>
+          </Row>
+          <Row className="project-description">
+            <p>{description}</p>
+          </Row>
+          <Row
+            md="auto"
+            sm="auto"
+            className="project-techs justify-content-center flex-nowrap"
+          >
+            {technologies && technologies.length > 0 ? (
+              technologies.map((item, index) => (
+                <Col key={index} className="text-center">
+                  <p>{item}</p>
+                </Col>
+              ))
+            ) : (
+              <p></p>
+            )}
+          </Row>
+
+          <Row
+            className={` align-items-end projects-buttons ${
+              showOnlyOneButton ? "justify-content-center" : ""
             }`}
-        >
-          {!isOnlineLinkEmpty && (
-            <Col xs={6} md={6}>
-              <a href={linkOnline} target="_blank" rel="noopener noreferrer">
-                <Button>Ver Online</Button>
-              </a>
-            </Col>
-          )}
-          {!isGitHubLinkEmpty && (
-            <Col xs={6} md={6}>
-              <a href={linkGitHub} target="_blank" rel="noopener noreferrer">
-                <Button>GitHub</Button>
-              </a>
-            </Col>
-          )}
-        </Row>
-      </Col>
-    </Row>
+          >
+            {!isOnlineLinkEmpty && (
+              <Col xs={6} md={6}>
+                <a href={linkOnline} target="_blank" rel="noopener noreferrer">
+                  <Button>Ver Online</Button>
+                </a>
+              </Col>
+            )}
+            {!isGitHubLinkEmpty && (
+              <Col xs={6} md={6}>
+                <a href={linkGitHub} target="_blank" rel="noopener noreferrer">
+                  <Button>GitHub</Button>
+                </a>
+              </Col>
+            )}
+          </Row>
+        </Col>
+      </Row>
     </Container>
   );
 };
